1942c04279e8b47d08be7e55318e31ece842a352
Shane Robiego <20191265@s.ubaguio.edu>
2024-04-16 09:43:24 +0800

    April 16, 2024

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+/*use Illuminate\Support\ServiceProvider;*/
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the "home" route for your application.
+     * 
+     * Typically, user are redirected here after authentication.
+     * 
+     * @var string
+     */
+
+    public const HOME = '/home';
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
