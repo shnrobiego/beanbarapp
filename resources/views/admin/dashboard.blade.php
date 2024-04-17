2ca423e0cab9183c328c7a7f34ab79bf1c8e606c
Shane Robiego <20191265@s.ubaguio.edu>
2024-04-17 15:40:03 +0800

    side bar menu created

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,32 +5,80 @@
         </h2>
     </x-slot>
 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+
+        <style>
+            aside {
+                position: absolute;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                width: 250px;
+                background-color: #263544;
+                color: #fff;
+                padding: 20px;
+                }
+        </style>
+        
+    </head>
+    <body class="font-sans antialiased">
+        <div class="relative min-h-screen md:flex">
+            <!--sidebar-->
+            <aside class="z-10 bg-blue-800 text-blue-100 w-64 px-2 py-4  inset-y-0">
+                <ul class="text-sm">
+                    <li class="px-4 py-2 hover:bg-gray-700">
+                        <a href="#" class="block">Dashboard</a>
+                    </li>
+                    <li class="px-4 py-2 hover:bg-gray-700">
+                        <a href="#" class="block">Cashier Management</a>
+                    </li>
+                    <li class="px-4 py-2 hover:bg-gray-700">
+                        <a href="#" class="block">Product Management</a>
+                    </li>
+                    <li class="px-4 py-2 hover:bg-gray-700">
+                        <a href="#" class="block">Sales Report</a>
+                    </li>
+                    <li class="px-4 py-2 hover:bg-gray-700">
+                        <a href="#" class="block">Settings</a>
+                    </li>
+                </ul>
+            </aside>
+        </div>
+    </body>
+    </html>
+
+    <!--
     <div class="flex">
-        <!-- Sidebar -->
+        
         <div class="bg-gray-800 text-white w-64 flex-none">
             <ul class="text-sm">
                 <li class="px-4 py-2 hover:bg-gray-700">
                     <a href="#" class="block">Dashboard</a>
                 </li>
                 <li class="px-4 py-2 hover:bg-gray-700">
-                    <a href="#" class="block">Cashier</a>
+                    <a href="#" class="block">Cashier Management</a>
                 </li>
                 <li class="px-4 py-2 hover:bg-gray-700">
-                    <a href="#" class="block">Product</a>
+                    <a href="#" class="block">Product Management</a>
                 </li>
                 <li class="px-4 py-2 hover:bg-gray-700">
-                    <a href="#" class="block">Sales</a>
+                    <a href="#" class="block">Sales Report</a>
                 </li>
                 <li class="px-4 py-2 hover:bg-gray-700">
                     <a href="#" class="block">Settings</a>
                 </li>
-                <!-- Add more menu items here -->
             </ul>
-        </div>
+        </div>-->
 
-        <!-- Content -->
+        <!-- Content 
         <div class="flex-1 p-4">
-            <!-- Add content for each menu item here -->
-        </div>
+            
+        </div>-->
     </div>
 </x-app-layout>
